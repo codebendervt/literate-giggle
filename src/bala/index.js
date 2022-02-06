6dda2a1d3dd39d4eb6bccd1775257e0a8f20be1c
codebenderhq <lionelmbankeu@gmail.com>
2022-02-06 12:57:39 +0200

    updated the experience

--- src/bala/index.js
+++ src/bala/index.js
@@ -8,18 +8,18 @@ const Bala  = () => {
     const [toggle, setToggle] = useState(false)
 
     return(
-        <div className={'w-screen h-screen flex flex-col  text-white select-none  items-center justify-center md:justify-center md:items-center text-gray-900 '}>
+        <div className={'w-screen h-screen flex flex-col  text-white select-none  items-center justify-center md:justify-center md:items-center text-gray-900 p-2'}>
 
             {
                 toggle ? <p className={'text-sm w-64 md:max-w-md md:w-full md:text-lg text-center font-book'}>
                     There is no greater agony than bearing an untold story inside you.  <br className={'md:hidden'}/>- Maya Angelou
-                </p> :   <div className={'max-w-7xl flex md:text-center  text-center justify-center font-black'}>
-                    <p className={"text-7xl lg:text-9xl text-blue-500"}>
+                </p> :   <div className={'w-64 md:max-w-7xl flex md:text-center  text-center justify-center font-black'}>
+                    <p className={"text-7xl md:text-8xl lg:text-9xl text-blue-500"}>
                         Write.
                         <br className={"md:hidden"}/>
-                        <span className={'text-8xl lg:text-9xl md:text-orange-500'}>Share.
+                        <span className={'text-8xl lg:text-9xl'}>Share.
                         </span> <br className={"md:hidden"}/>
-                        <span className={'text-8xl md:text-6xl lg:text-9xl text-orange-500 md:text-blue-500'}>Publish.</span>
+                        <span className={'text-8xl md:text-8xl lg:text-9xl text-orange-500'}>Publish.</span>
                     </p>
                 </div>
             }
@@ -30,7 +30,7 @@ const Bala  = () => {
                 <a href={'/list'} className={'p-2 rounded bg-purple-500 text-white mx-2 '}>Join Waiting List</a>
             </div>
 
-            <div className={'max-w-2xl w-full flex justify-center py-2'}>
+            <div className={'max-w-2xl w-full flex justify-center py-2 font-black'}>
                 <div className={'text-purple-500 text-lg cursor-pointer'} onClick={() =>  setToggle(!toggle)}>{toggle ? 'Learn Less' : 'Learn More'} </div>
             </div>
 
