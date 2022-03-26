f57241eca25db5ddeefb136069f920af7ec4ac3e
codebenderhq <lionelmbankeu@gmail.com>
2022-03-26 13:37:04 +0200

    made the logger more robust

--- bridge/api/logger/index.ts
+++ bridge/api/logger/index.ts
@@ -5,12 +5,14 @@ type Log = {
     title : string,
     msg: string,
     count: number
+    msgs: string[]
 }
 
 const index = async (data: Log, req: any) => {
 
     try{
         data.count = 0;
+        data.msgs = [data.msg]
         await create(data,'log')
 
         return {status:200, msg: 'Logged'}
@@ -19,6 +21,7 @@ const index = async (data: Log, req: any) => {
         if(err.message == 'instance not unique'){
             const _data = await  findByIndex(data.title,'title')
             _data[0].data.count = data.count + 1
+            _data[0].data.msgs.push(data.msg)
             await update(_data[0].data,_data[0].ref.id,'log')
 
             return {status:200, msg: 'Log updated'}
