d5224c178d8e44da0586db299b03a8ba81b3ee0a
codebenderhq <lionelmbankeu@gmail.com>
2022-04-04 19:50:06 +0200

    my goodness package dev

--- sdk/studio/style/assets/icons/index.js
+++
@@ -1,9 +0,0 @@
-import addIcon from './add.svg';
-import next_icon from 'url:./next.svg';
-import alertIcon from './alert.svg';
-import shareIcon from './share.svg';
-import submit_icon from 'url:./submit.svg';
-import upload_icon from 'url:./upload.svg';
-import loader_icon from 'url:./loader.svg';
-
-export default {next_icon,submit_icon,upload_icon,loader_icon}
