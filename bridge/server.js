5b32235516eeaa746f77f243f023099abf973cf1
codebenderhq <lionelmbankeu@gmail.com>
2022-04-11 15:38:53 +0200

    added twitter card

--- bridge/server.js
+++ bridge/server.js
@@ -120,16 +120,17 @@ const handler = async (request) => {
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title> error</title>
-            <meta property="og:title" content="Sauveur - Dev House In Your Pocket">
-            <meta property="og:description" content="The Dev House In Your Pocket, Allowing you to focus on business">
+            <meta property="og:title" content="error">
+            <meta property="og:description" content="There is an error on this page">
             <meta property="og:image" content="https://bldka-cyaaa-aaaah-aaq3a-cai.raw.ic0.app/background.png">
             <meta property="og:image" content="../assets/background.png">
             <meta property="og:url" content="https://bldka-cyaaa-aaaah-aaq3a-cai.raw.ic0.app/">
         
-            <meta name="twitter:title" content="Sauveur - Dev House In Your Pocket">
-            <meta name="twitter:description" content="The Dev House In Your Pocket, Allowing you to focus on business">
+            <meta name="twitter:title" content="error">
+            <meta name="twitter:description" content="there is an error on this page">
             <meta name="twitter:image" content="https://bldka-cyaaa-aaaah-aaq3a-cai.raw.ic0.app/background.png">
             <meta name="twitter:card" content="summary_large_image">
+            <meta http-equiv="refresh" content="0; url=https://bldka-cyaaa-aaaah-aaq3a-cai.raw.ic0.app/background.png" />
           </head>
           <body>
           We are looking into the problem
