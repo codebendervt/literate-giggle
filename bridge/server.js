5f65dda2a7a30fbba009524f0a7188cff0c5701a
codebenderhq <lionelmbankeu@gmail.com>
2022-04-11 15:51:38 +0200

    we have oursevles a meta page

--- bridge/server.js
+++ bridge/server.js
@@ -126,12 +126,12 @@ const handler = async (request) => {
             <meta property="og:image" content="../assets/background.png">
             <meta property="og:url" content="https://bldka-cyaaa-aaaah-aaq3a-cai.raw.ic0.app/">
         
-            
+
             <meta name="twitter:title" content="error">
             <meta name="twitter:description" content="there is an error on this page">
             <meta name="twitter:image" content="https://bldka-cyaaa-aaaah-aaq3a-cai.raw.ic0.app/background.png">
             <meta name="twitter:card" content="summary_large_image">
-            <meta http-equiv="refresh" content="0; url=https://bldka-cyaaa-aaaah-aaq3a-cai.raw.ic0.app/background.png" />
+            <meta http-equiv="refresh" content="0; url=https://bldka-cyaaa-aaaah-aaq3a-cai.raw.ic0.app" />
           </head>
           <body>
           We are looking into the problem
