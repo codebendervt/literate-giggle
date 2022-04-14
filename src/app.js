33e64c450fedd6227d7afdfa15c484b95e0bbd23
codebenderhq <lionelmbankeu@gmail.com>
2022-04-14 16:03:28 +0200

    the build needs work damn

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 import { render } from "react-dom";
 // import {useEffect,useState} from "react";
-import 'animate.css';
-import App from "../archive/toolbox";
-import pages from './'
+// import 'animate.css';
+// import App from "../archive/toolbox";
+// import pages from './'
 
 
 const Layout = () => {
 
     return(
 
-        <App pages={pages}></App>
+        <div>hello world</div>
+        // <App pages={pages}></App>
 
         )
 
