f7737ae892e08b21058797089f8fc71a52aa97c6
codebenderhq <lionelmbankeu@gmail.com>
2022-04-18 23:49:15 +0200

    something is up

--- playground/index.js
+++ playground/index.js
@@ -17,80 +17,9 @@ const Layout = () => {
 
     useEffect(async () => {
 
-        console.log(actions.auth.log_in())
-        // const myArray = new Uint8Array(10);
-
-        // const msg = {data:'hellow world'}
-        
-        // let _encrypted
-        // //Uint8Array
-        // _encrypted =  await encryptMessage(JSON.stringify(msg));
-        // // console.log(await decryptMessage(_encrypted));
-
-        // console.log(_encrypted)
-        // fetch('/push', {
-        //     method: 'POST',
-        //     body: _encrypted
-        // })
-
-        // const socket = new WebSocket("ws://clear-donkey-10-7cvv1r7bn5q0.deno.dev");
-        //
-        //
-        // // Connection opened
-        // socket.addEventListener('open', function (event) {
-        //     socket.send('Hello Server Again!');
-        // });
-        //
-        // // Listen for messages
-        // socket.addEventListener('message', function (event) {
-        //     console.log('Message from server ', event.data);
-        // });
-
-        // const subscription = await push_notification()
-        // setSub(subscription)
-
-
     },[])
 
-    function checkNotificationPromise() {
-        try {
-            Notification.requestPermission().then();
-        } catch(e) {
-            return false;
-        }
-
-        return true;
-    }
-
-    function askNotificationPermission() {
-        // function to actually ask the permissions
-        function handlePermission(permission) {
-            // set the button to shown or hidden, depending on what the user answers
-            if(Notification.permission === 'denied' || Notification.permission === 'default') {
-                console.log('denied');
-            } else {
-                var img = '/to-do-notifications/img/icon-128.png';
-                var text = 'HEY! Your task "' + 'twek' + '" is now overdue.';
-                var notification = new Notification('To do list', { body: text, icon: img });
-            }
-        }
-
-        // Let's check if the browser supports notifications
-        if (!('Notification' in window)) {
-            console.log("This browser does not support notifications.");
-        } else {
-            if(checkNotificationPromise()) {
-                Notification.requestPermission()
-                    .then((permission) => {
-                        handlePermission(permission);
-                    })
-            } else {
-                Notification.requestPermission(function(permission) {
-                    handlePermission(permission);
-                });
-            }
-        }
-    }
+ 
 
 
     const handleSubmit = () => {
